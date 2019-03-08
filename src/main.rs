ee51c55fb3e645e723b835ae13c666bc213032f3
Clifford T. Matthews <ctm@devctm.com>
2019-03-07 17:26:08 -0700

    Updated my resume (although it's still pretty poor) and updated copy on site.
    A step in the right direction, I guess, but oh do I find this stuff
    unpleasant.

--- src/main.rs
+++ src/main.rs
@@ -51,74 +51,73 @@ fn index(_req: &HttpRequest) -> Markup {
                                 h1 { "devctm" }
                                 p {
                                     r#"
-I'm Cliff Matthews, a professional programmer since 1978 and an independent
-consultant since 1985.  This is my "Cobbler's children's shoes" web-site.
+I'm Cliff Matthews, a professional programmer since 1978 and contractor since 1985.
                                     "#
                                 }
                                 p {
                                     r#"
-I got my MSCS from the University of New Mexico in 1985, applied to UC
-Berkeley's Ph.D. program and didn't get in, so I returned to industry.
+I earned my MSCS from the University of New Mexico in 1985, applied to UC
+Berkeley's Ph.D. program, didn't get in, and returned to industry.
                                     "#
                                 }
                                 p {
                                     r#"
-In 1989, I founded Abacus Research & Development. We did a
+In 1989, I founded Abacus Research & Development. We wrote a
                                     "#
                                     a href="https://archive.org/details/executor" {
                                         r#"
     clean-room reimplementation of the Macintosh ROM and portions of Mac OS"#
+                                            "."
                                     }
-                                    r#".
-Technically what we did was very advanced for the time, but I was a poor CEO.
-Consequentially, I occasionally returned to consulting to fund ARDI.
+                                }
+                                p {
+                                    r#"
+At ARDI, I got interested in poker. Mat Hostetter's keen
+insight led the two of us to writing what was then the world's
+fastest poker hand evaluator ("#
+a href="https://pdfs.semanticscholar.org/aa66/62b2092e148e7db7004d2030294df51fcb2f.pdf" { "footnotes 113 & 114 on p. 44" } ")."
+                                }
+                                p {
+                                    r#"
+On Halloween 1998, I wrote the core
+of "multibot", the first internet server to deal multi-table poker
+tournaments.  I sold multibot to Poker School Online who
+were eventually bought by PokerStars.
                                     "#
                                 }
                                 p {
                                     r#"
-During my ARDI years, I got interested in poker. Mat Hostetter had a keen
-insight that led to the two of us writing what was then (~1994) the world's
-fastest poker hand evaluator, which we released as free software.
-                                    "#
+Multibot was Objective-C, without a framework, just the GNU
+ Objective-C runtime.  Multihreaded, using epoll it was very
+efficient. However, when PSO upgraded to a dual processor, they had
+an unexplained crash. Wow. I discovered "#
+                                        a href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=9969" { "a race condition" }
+                                    " in the Objective-C runtime."
                                 }
                                 p {
                                     r#"
-On Halloween 1998, I was stuck at home handing out treats so I wrote the core
-of what was to become "multibot", the first software to deal multi-table poker
-tournaments on the internet.  I sold multibot to Poker School Online and
-eventually they were bought by PokerStars.
-                                    "#
-                                }
-                                p {
-                                    r#"
-In 2006, I co-founded Stolen Bases LLC, a company to leverage Ruby on Rails.
-Although our headquarters were in Manhatten, I put together a development team
-in Albuquerque.
-                                    "#
-                                }
-                                p {
-                                    " In 2009, "
+In 2006, I co-founded Stolen Bases LLC, a Ruby on Rails shop.  
+Although headquartered in Manhatten, I put together a development team
+in Albuquerque.  In 2009, "#
                                     a href="https://elinemedia.com" { "E-Line Media" }
                                     " bought Stolen Bases."
                                 }
                                 p {
                                     "Since then, I've been doing business as "
                                     span.devctm { "devctm" }
-                                    r#".
-The name was meant to imply that I'm a coding machine, but almost all my
-work has been proprietary in private repositories, so I don't have much code
+                                    r#", a name meant to imply that I'm a coding "machine". However with almost all of my
+recent work in private repositories, I don't have much code
 to show.  On the other hand, I started running in 2009 and have participated
 in many
                                     "#
                                     a href="https://ultrasignup.com/results_participant.aspx?fname=Clifford&lname=Matthews" { "running events" }
                                     r#", so perhaps we can stipulate that I'm
-a running machine.
+a running machine and that I am driven.
                                     "#
                                 }
                                 p {
                                     r#"
-FWIW, this toy site recycles some copy and styling from its previous
-incarnation in Ruby on Rails, but is a tiny Rust program using
+This site is a tiny Rust program using
                                     "#
                                     a href="https://actix.rs" { "actix" }
                                     ", "
@@ -144,12 +143,22 @@ incarnation in Ruby on Rails, but is a tiny Rust program using
                                 }
                             }
                             div#resumes {
-                                h1 { (PreEscaped("&nbsp")) }
+                                h1 { (PreEscaped("Links")) }
                                 div.wrapper {
                                     ul {
                                         li {
                                             a href="/assets/resume.pdf" {
-                                                "Cliff's Resume"
+                                                "Resume"
+                                            }
+                                        }
+                                        li {
+                                            a href="https://github.com/ctm" {
+                                                "GitHub"
+                                            }
+                                        }
+                                        li {
+                                            a href="https://www.builtinnm.org/people/20" {
+                                                "Built in NM"
                                             }
                                         }
                                     }
