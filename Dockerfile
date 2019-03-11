6bd6365a8d705dbd2037ecf3cbf096aa3745e611
Clifford T. Matthews <ctm@devctm.com>
2019-03-11 14:29:51 -0600

    Let's Encrypt Proof-of-concept.  This version is running on my QNap and successfully serving devctm.com, ardi.com, sceim.net and test.devctm.com.
    The Let's Encrypt support is now in a separate repository.  I'm not
    happy with the API I'm exposing, but I've started a dialog toward
    making it better.

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,8 @@ WORKDIR /usr/src/devctm
 COPY ./ ./
 
 RUN cargo build --release
-RUN cargo install acme-client
 RUN mkdir -p /build-out
-RUN cp target/release/devctm /usr/local/cargo/bin/acme-client /build-out/
+RUN cp target/release/devctm /build-out/
 
 FROM ubuntu
 
@@ -14,8 +13,6 @@ ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update && apt-get -y install ca-certificates libssl-dev && rm -rf /var/lib/apt/lists/*
 COPY --from=build /build-out/devctm /
 RUN mkdir -p /usr/local/cargo/bin -p /ssl
-COPY --from=build /build-out/acme-client /usr/local/cargo/bin/
-COPY --from=build /usr/src/devctm/ssl/ /ssl/
 
 RUN groupadd -g 999 devctm && useradd -r -u 999 -g devctm devctm
 RUN mkdir -p /var/devctm && chown -R 999 /ssl /var/devctm && chmod 700 /var/devctm
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 #![feature(proc_macro_hygiene)]
 
-mod acme;
-
 include!(concat!(env!("OUT_DIR"), "/statics.rs"));
 
 use {
     actix_web::{
-        error::ErrorNotFound, http::Method, middleware::Logger, server, App, HttpRequest,
-        HttpResponse, Result,
+        actix::Actor, error::ErrorNotFound, http::Method, middleware::Logger, server, App,
+        HttpRequest, HttpResponse, Result,
     },
+    actix_web_lets_encrypt::{CertBuilder, LetsEncrypt},
     listenfd::ListenFd,
     maud::{html, Markup, PreEscaped, DOCTYPE},
-    openssl::ssl::{SslAcceptor, SslAcceptorBuilder, SslFiletype, SslMethod},
     std::{env, path::PathBuf},
 };
 
@@ -33,132 +31,133 @@ fn image_tag(filename: &str, height: u16, width: u16, alt: &str) -> Markup {
 
 fn index(_req: &HttpRequest) -> Markup {
     html! {
-        (DOCTYPE)
-            html {
-                meta charset="utf-8";
-                head {
-                    title { "Cliff's Toy Rust (Actix) Server" }
-                    ( stylesheet_link_tag("application", "all") )
-                }
-                body#more {
-                    div#container {
-                        div#header {
-                          ( image_tag("devctm_logo.gif", 24, 100, "devctm") )
+            (DOCTYPE)
+                html {
+                    meta charset="utf-8";
+                    head {
+                        title { "Cliff's Toy Rust (Actix) Server" }
+                        ( stylesheet_link_tag("application", "all") )
+                    }
+                    body#more {
+                        div#container {
+                            div#header {
+                              ( image_tag("devctm_logo.gif", 24, 100, "devctm") )
 
-                        }
-                        div#main-body {
-                            div#devctm {
-                                h1 { "devctm" }
-                                p {
-                                    r#"
+                            }
+                            div#main-body {
+                                div#devctm {
+                                    h1 { "devctm" }
+                                    p {
+                                        r#"
 I'm Cliff Matthews, a professional programmer since 1978 and contractor since 1985.
                                     "#
-                                }
-                                p {
-                                    r#"
+                                    }
+                                    p {
+                                        r#"
 I earned my MSCS from the University of New Mexico in 1985, applied to UC
 Berkeley's Ph.D. program, didn't get in, and returned to industry.
                                     "#
-                                }
-                                p {
-                                    r#"
+                                    }
+                                    p {
+                                        r#"
 In 1989, I founded Abacus Research & Development. We wrote a
                                     "#
-                                    a href="https://archive.org/details/executor" {
-                                        r#"
-    clean-room reimplementation of the Macintosh ROM and portions of Mac OS"#
-                                            "."
+                                        a href="https://archive.org/details/executor" {
+                                            r#"
+        clean-room reimplementation of the Macintosh ROM and portions of Mac OS"#
+                                                "."
+                                        }
                                     }
-                                }
-                                p {
-                                    r#"
+                                    p {
+                                        r#"
 At ARDI, I got interested in poker. Mat Hostetter's keen
 insight led the two of us to writing what was then the world's
 fastest poker hand evaluator ("#
-a href="https://pdfs.semanticscholar.org/aa66/62b2092e148e7db7004d2030294df51fcb2f.pdf" { "footnotes 113 & 114 on p. 44" } ")."
-                                }
-                                p {
-                                    r#"
+    a href="https://pdfs.semanticscholar.org/aa66/62b2092e148e7db7004d2030294df51fcb2f.pdf" { "footnotes 113 & 114 on p. 44" } ")."
+                                    }
+                                    p {
+                                        r#"
 On Halloween 1998, I wrote the core
 of "multibot", the first internet server to deal multi-table poker
 tournaments.  I sold multibot to Poker School Online who
 were eventually bought by PokerStars.
                                     "#
-                                }
-                                p {
-                                    r#"
+                                    }
+                                    p {
+                                        r#"
 Multibot was Objective-C, without a framework, just the GNU
  Objective-C runtime.  Multihreaded, using epoll it was very
 efficient. However, when PSO upgraded to a dual processor, they had
 an unexplained crash. Wow. I discovered "#
-                                        a href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=9969" { "a race condition" }
-                                    " in the Objective-C runtime."
-                                }
-                                p {
-                                    r#"
+                                            a href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=9969" { "a race condition" }
+                                        " in the Objective-C runtime."
+                                    }
+                                    p {
+                                        r#"
 In 2006, I co-founded Stolen Bases LLC, a Ruby on Rails shop.  
 Although headquartered in Manhatten, I put together a development team
 in Albuquerque.  In 2009, "#
-                                    a href="https://elinemedia.com" { "E-Line Media" }
-                                    " bought Stolen Bases."
-                                }
-                                p {
-                                    "Since then, I've been doing business as "
-                                    span.devctm { "devctm" }
-                                    r#", a name meant to imply that I'm a coding "machine". However with almost all of my
+                                        a href="https://elinemedia.com" { "E-Line Media" }
+                                        " bought Stolen Bases."
+                                    }
+                                    p {
+                                        "Since then, I've been doing business as "
+                                        span.devctm { "devctm" }
+                                        r#", a name meant to imply that I'm a coding "machine". However with almost all of my
 recent work in private repositories, I don't have much code
 to show.  On the other hand, I started running in 2009 and have participated
 in many
                                     "#
-                                    a href="https://ultrasignup.com/results_participant.aspx?fname=Clifford&lname=Matthews" { "running events" }
-                                    r#", so perhaps we can stipulate that I'm
+                                        a href="https://ultrasignup.com/results_participant.aspx?fname=Clifford&lname=Matthews" { "running events" }
+                                        r#", so perhaps we can stipulate that I'm
 a running machine and that I am driven.
                                     "#
-                                }
-                                p {
-                                    r#"
+                                    }
+                                    p {
+                                        r#"
 This site is a tiny Rust program using
                                     "#
-                                    a href="https://actix.rs" { "actix" }
-                                    ", "
-                                    a href="https://maud.lambda.xyz" { "maud" }
-                                    " and "
-                                    a href="https://github.com/kaj/rsass" { "rsass" }
-                                    "."
+                                        a href="https://actix.rs" { "actix" }
+                                        ", "
+                                        a href="https://maud.lambda.xyz" { "maud" }
+                                        " and "
+                                        a href="https://github.com/kaj/rsass" { "rsass" }
+                                        "."
+                                    }
                                 }
-                            }
-                            div#contact {
-                                h1 { "Availability and Rates" }
-                                div.wrapper {
-                                    p {
-                                        "My rate for Ruby is $200/hr."
-                                        br; br;
-                                        "My rate for Rust is $100/hr. through September 30th, 2019."
-                                        br; br;
-                                        "If interested, please email "
-                                        a href="mailto:ctm@devctm.com" {
-                                            "ctm@devctm.com"
+                                div#contact {
+                                    h1 { "Availability and Rates" }
+                                    div.wrapper {
+                                        p {
+                                            "My rate for Ruby is $200/hr."
+                                            br; br;
+                                            "My rate for Rust is $100/hr. through September 30th, 2019."
+                                            br; br;
+                                            "If interested, please email "
+                                            a href="mailto:ctm@devctm.com" {
+                                                "ctm@devctm.com"
+                                            }
                                         }
                                     }
                                 }
-                            }
-                            div#resumes {
-                                h1 { (PreEscaped("Links")) }
-                                div.wrapper {
-                                    ul {
-                                        li {
-                                            a href="/assets/resume.pdf" {
-                                                "Resume"
+                                div#resumes {
+                                    h1 { (PreEscaped("Links")) }
+                                    div.wrapper {
+                                        ul {
+                                            li {
+                                                a href="/assets/resume.pdf" {
+                                                    "Resume"
+                                                }
                                             }
-                                        }
-                                        li {
-                                            a href="https://github.com/ctm" {
-                                                "GitHub"
+                                            li {
+                                                a href="https://github.com/ctm" {
+                                                    "GitHub"
+                                                }
                                             }
-                                        }
-                                        li {
-                                            a href="https://www.builtinnm.org/people/20" {
-                                                "Built in NM"
+                                            li {
+                                                a href="https://www.builtinnm.org/people/20" {
+                                                    "Built in NM"
+                                                }
                                             }
                                         }
                                     }
@@ -167,8 +166,7 @@ This site is a tiny Rust program using
                         }
                     }
                 }
-            }
-    }
+        }
 }
 
 fn asset(req: &HttpRequest) -> Result<HttpResponse> {
@@ -207,44 +205,55 @@ fn content_type(asset: &PathBuf) -> &'static str {
     CONTENT_TYPES.get(extension).unwrap()
 }
 
-fn ssl_builder(host: &str) -> SslAcceptorBuilder {
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file(format!("ssl/{}_key.pem", host), SslFiletype::PEM)
-        .unwrap();
-    builder
-        .set_certificate_chain_file(format!("ssl/{}_cert.pem", host))
-        .unwrap();
-    builder
-}
-
 fn main() {
     env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
     let mut listenfd = ListenFd::from_env();
 
-    let mut server = server::new(|| {
-        App::new()
-            .middleware(Logger::default())
-            .resource("/assets/{asset:.*}", |r| r.method(Method::GET).f(asset))
-            .resource("/", |r| r.method(Method::GET).f(index))
-            .resource("/.well-known/acme-challenge/{token}", |r| {
-                r.method(Method::GET).f(acme::nonce)
-            })
+    // TODO: get port config from environment variables so that I can
+    //       build a container and test it without having to take the
+    //       production container down.  OTOH, this mix of certs
+    //       allowed me to end-to-end test that I do properly support
+    //       multiple domains in a single cert and certs coming from
+    //       Let's Encrypt's test server, so I have that going for me.
+    let devctm_prod = CertBuilder::new("0.0.0.0:8089", &["devctm.com"]).email("ctm@devctm.com");
+
+    let ardi_prod =
+        CertBuilder::new("0.0.0.0:8090", &["ardi.com", "sceim.net"]).email("ctm@ardi.com");
+
+    let devctm_test = CertBuilder::new("0.0.0.0:8091", &["test.devctm.com"])
+        .email("ctm@devctm.com")
+        .test();
+
+    // 8088 is for all http and is bound after we set up the server.
+    let app_encryption_enabler = LetsEncrypt::encryption_enabler()
+        .nonce_directory("/var/devctm")
+        .ssl_directory("ssl")
+        .add_cert(devctm_prod)
+        .add_cert(ardi_prod)
+        .add_cert(devctm_test);
+
+    let server_encryption_enabler = app_encryption_enabler.clone();
+
+    let mut server = server::new(move || {
+        App::new().configure(|app| {
+            let app = app
+                .middleware(Logger::default())
+                .resource("/assets/{asset:.*}", |r| r.method(Method::GET).f(asset))
+                .resource("/", |r| r.method(Method::GET).f(index));
+            app_encryption_enabler.register(app)
+        })
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)
     } else {
-        server
+        server_encryption_enabler
+            .attach_certificates_to(server)
             .bind("0.0.0.0:8088")
             .unwrap()
-            .bind_ssl("0.0.0.0:8089", ssl_builder("devctm"))
-            .unwrap()
-            .bind_ssl("0.0.0.0:8090", ssl_builder("ardi"))
-            .unwrap()
     };
-
+    server_encryption_enabler.start();
     server.run();
 }
