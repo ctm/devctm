f062348a9376a86a1748b652db085bf292613b74
Clifford T. Matthews <ctm@devctm.com>
2019-04-02 11:13:27 -0600

    chore: updated devctm copy
    What I have now is too wordy, although I think it is an improvement.  I
    have mixed feelings about including my PSO/PokerStars handshake.  Perhaps
    I'll cut it.  I was tempted to include the time Steve Jobs called me at
    home (when I was incapacitated by pneumonia), but I'm pretty eager to
    wrap this up and get back to coding.

--- src/views/devctm.rs
+++ src/views/devctm.rs
@@ -6,134 +6,150 @@ use {
 
 pub fn index(_req: &HttpRequest) -> Markup {
     html! {
-            (DOCTYPE)
-                html {
-                    meta charset="utf-8";
-                    head {
-                        title { "Cliff's Toy Rust (Actix) Server" }
-                        ( stylesheet_link_tag("application", "all") )
+        (DOCTYPE)
+        html {
+            meta charset="utf-8";
+            head {
+                title { "Cliff's Toy Rust (Actix) Server" }
+                ( stylesheet_link_tag("application", "all") )
+            }
+            body#more {
+                div#container {
+                    div#header {
+                        ( image_tag("devctm_logo.gif", 24, 100, "devctm") )
                     }
-                    body#more {
-                        div#container {
-                            div#header {
-                              ( image_tag("devctm_logo.gif", 24, 100, "devctm") )
-
+                    div#main-body {
+                        div#devctm {
+                            h1 { "devctm" }
+                            p {
+"I'm Cliff Matthews, a contract programmer."
                             }
-                            div#main-body {
-                                div#devctm {
-                                    h1 { "devctm" }
-                                    p {
-                                        r#"
-I'm Cliff Matthews, a professional programmer since 1978 and contractor since 1985.
-                                    "#
-                                    }
-                                    p {
-                                        r#"
-I earned my MSCS from the University of New Mexico in 1985, applied to UC
-Berkeley's Ph.D. program, didn't get in, and returned to industry.
-                                    "#
-                                    }
-                                    p {
-                                        r#"
-In 1989, I founded Abacus Research & Development. We wrote a
-                                    "#
-                                        a href="https://archive.org/details/executor" {
-                                            r#"
-        clean-room reimplementation of the Macintosh ROM and portions of Mac OS"#
-                                                "."
-                                        }
-                                    }
-                                    p {
-                                        r#"
-At ARDI, I got interested in poker. Mat Hostetter's keen
-insight led the two of us to writing what was then the world's
-fastest poker hand evaluator ("#
-    a href="https://pdfs.semanticscholar.org/aa66/62b2092e148e7db7004d2030294df51fcb2f.pdf" { "footnotes 113 & 114 on p. 44" } ")."
-                                    }
-                                    p {
-                                        r#"
-On Halloween 1998, I wrote the core
-of "multibot", the first internet server to deal multi-table poker
-tournaments.  I sold multibot to Poker School Online who
-were eventually bought by PokerStars.
-                                    "#
-                                    }
-                                    p {
-                                        r#"
-Multibot was Objective-C, without a framework, just the GNU
- Objective-C runtime.  Multihreaded, using epoll it was very
-efficient. However, when PSO upgraded to a dual processor, they had
-an unexplained crash. Wow. I discovered "#
-                                            a href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=9969" { "a race condition" }
-                                        " in the Objective-C runtime."
-                                    }
-                                    p {
-                                        r#"
+                            p {
+                                (PreEscaped(
+r#"Perhaps you're here because you're considering becoming my client,
+or&mdash;for all I know&mdash;this page has been linked to as an
+example of what not to do. Regardless, I'm having fun and am glad you
+dropped by."#
+                                        ))
+                            }
+                            p {
+r#"I'm an honest, loyal, driven person with sufficient experience to help you
+sleep at night."#
+                            }
+                            p {
+r#"Born in late 1962, I got into programming via the August 1976
+Popular Electronics "#
+                                a href="https://billr.incolor.com/elf/html/elf-1-33.htm" { r#"COSMAC "Elf" article"# }
+r#". By 1978, I was paid to write PDP-8 assembly.  I 
+got my Masters Degree in Computer Science from the University of New Mexico
+in 1985 and I've been an entrepreneur and contractor ever since."#
+                            }
+                            p {
+"In 1989, I founded Abacus Research & Development. We wrote a "
+                                a href="https://archive.org/details/executor" {
+"clean-room reimplementation of the Macintosh ROM and portions of Mac OS"
+                                }
+r#".  I wrote the first 10,000 lines (in C) of ROMlib on a diskless Mac+ that
+only had a single floppy drive."#
+                            }
+                            p {
+r#"At ARDI, I got interested in poker. Mat Hostetter's keen insight led the
+two of us to writing what was then the world's fastest poker hand evaluator ("#
+                                a href="https://pdfs.semanticscholar.org/aa66/62b2092e148e7db7004d2030294df51fcb2f.pdf" { "footnotes 113 & 114 on p. 44" } ")."
+                            }
+                            p {
+r#"On Halloween 1998, I wrote the core of "multibot", the first internet
+server to deal multi-table poker tournaments.  I sold multibot to Poker School
+Online who were eventually bought by PokerStars."#
+                            }
+                            p {
+r#"FWIW, one of
+the founders of PokerStars had actually heard about multibot between when I
+had agreed to sell it to PSO and when I received my check. PokerStars
+offered to pay me twice what I had already agreed on and I turned it down,
+because I had already shaken hands on the deal."#
+                            }
+                            p {
+r#"Multibot was Objective-C, without a framework, just the GNU  Objective-C
+runtime.  Multihreaded, using epoll it was very efficient. However, when PSO
+upgraded to a dual processor, they had an unexplained crash. Wow. I
+discovered "#
+                                a href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=9969" { "a race condition" }
+" in the Objective-C runtime."
+                            }
+                            p {
+                                r#"
 In 2006, I co-founded Stolen Bases LLC, a Ruby on Rails shop.  
-Although headquartered in Manhatten, I put together a development team
+Although headquartered in Manhattan, I put together a development team
 in Albuquerque.  In 2009, "#
-                                        a href="https://elinemedia.com" { "E-Line Media" }
-                                        " bought Stolen Bases."
-                                    }
-                                    p {
-                                        "Since then, I've been doing business as "
-                                        span.devctm { "devctm" }
-                                        r#", a name meant to imply that I'm a coding "machine". However with almost all of my
+                                    a href="https://elinemedia.com" { "E-Line Media" }
+                                " bought Stolen Bases."
+                            }
+                            p {
+                                "Since then, I've been doing business as "
+                                    span.devctm { "devctm" }
+                                r#", a name meant to imply that I'm a coding "machine". However with almost all of my
 recent work in private repositories, I don't have much code
 to show.  On the other hand, I started running in 2009 and have participated
 in many
                                     "#
-                                        a href="https://ultrasignup.com/results_participant.aspx?fname=Clifford&lname=Matthews" { "running events" }
-                                        r#", so perhaps we can stipulate that I'm
+                                    a href="https://ultrasignup.com/results_participant.aspx?fname=Clifford&lname=Matthews" { "running events" }
+                                r#", so perhaps we can stipulate that I'm
 a running machine and that I am driven.
                                     "#
-                                    }
-                                    p {
-                                        r#"
-This site is a tiny Rust program using
-                                    "#
-                                        a href="https://actix.rs" { "actix" }
-                                        ", "
-                                        a href="https://maud.lambda.xyz" { "maud" }
-                                        " and "
-                                        a href="https://github.com/kaj/rsass" { "rsass" }
-                                        "."
-                                    }
+                            }
+                            p {
+r#"I'm currently seeking contract work as a Rust programmer, even though I'm
+new to the language and not particularly good or efficient.  As such, for now,
+I'm better suited to a supporting role rather than a lead architect.  I'm
+inspired by what some people are doing with Rust and I'll certainly be doing
+a lot of side projects in Rust, even if I have to program in Ruby (which I
+still love) to pay the bills."#
+                            }
+                            p {
+                                "This page is a "
+                                a href="https://github.com/ctm/devctm" { "tiny Rust program" }
+" using "
+                                    a href="https://actix.rs" { "actix" }
+                                ", "
+                                    a href="https://maud.lambda.xyz" { "maud" }
+                                " and "
+                                    a href="https://github.com/kaj/rsass" { "rsass" }
+                                "."
+                            }
+                        }
+                        div#contact {
+                            h1 { "Availability and Rates" }
+                            div.wrapper {
+                                p {
+                                    "My rate for Ruby is $200/hr."
+                                        br; br;
+                                    "My rate for Rust is $100/hr. through September 30th, 2019."
+                                        br; br;
+                                    "If interested, please email "
+                                        a href="mailto:ctm@devctm.com" {
+                                            "ctm@devctm.com"
+                                        }
                                 }
-                                div#contact {
-                                    h1 { "Availability and Rates" }
-                                    div.wrapper {
-                                        p {
-                                            "My rate for Ruby is $200/hr."
-                                            br; br;
-                                            "My rate for Rust is $100/hr. through September 30th, 2019."
-                                            br; br;
-                                            "If interested, please email "
-                                            a href="mailto:ctm@devctm.com" {
-                                                "ctm@devctm.com"
-                                            }
+                            }
+                        }
+                        div#resumes {
+                            h1 { (PreEscaped("Links")) }
+                            div.wrapper {
+                                ul {
+                                    li {
+                                        a href="/assets/resume.pdf" {
+                                            "Resume"
                                         }
                                     }
-                                }
-                                div#resumes {
-                                    h1 { (PreEscaped("Links")) }
-                                    div.wrapper {
-                                        ul {
-                                            li {
-                                                a href="/assets/resume.pdf" {
-                                                    "Resume"
-                                                }
-                                            }
-                                            li {
-                                                a href="https://github.com/ctm" {
-                                                    "GitHub"
-                                                }
-                                            }
-                                            li {
-                                                a href="https://www.builtinnm.org/people/20" {
-                                                    "Built in NM"
-                                                }
-                                            }
+                                    li {
+                                        a href="https://github.com/ctm" {
+                                            "GitHub"
+                                        }
+                                    }
+                                    li {
+                                        a href="https://www.builtinnm.org/people/20" {
+                                            "Built in NM"
                                         }
                                     }
                                 }
@@ -141,5 +157,7 @@ This site is a tiny Rust program using
                         }
                     }
                 }
+            }
         }
+    }
 }
