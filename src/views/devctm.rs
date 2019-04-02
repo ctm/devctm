f9007fec7512455d60c8ae6442482eb8fdb96e9c
Clifford T. Matthews <ctm@devctm.com>
2019-04-02 12:40:30 -0600

    chore: pared down devctm copy
    It's still too wordy, but good enough for now.

--- src/views/devctm.rs
+++ src/views/devctm.rs
@@ -22,15 +22,7 @@ pub fn index(_req: &HttpRequest) -> Markup {
                         div#devctm {
                             h1 { "devctm" }
                             p {
-"I'm Cliff Matthews, a contract programmer."
-                            }
-                            p {
-                                (PreEscaped(
-r#"Perhaps you're here because you're considering becoming my client,
-or&mdash;for all I know&mdash;this page has been linked to as an
-example of what not to do. Regardless, I'm having fun and am glad you
-dropped by."#
-                                        ))
+"I'm Cliff Matthews, programmer."
                             }
                             p {
 r#"I'm an honest, loyal, driven person with sufficient experience to help you
@@ -41,8 +33,8 @@ r#"Born in late 1962, I got into programming via the August 1976
 Popular Electronics "#
                                 a href="https://billr.incolor.com/elf/html/elf-1-33.htm" { r#"COSMAC "Elf" article"# }
 r#". By 1978, I was paid to write PDP-8 assembly.  I 
-got my Masters Degree in Computer Science from the University of New Mexico
-in 1985 and I've been an entrepreneur and contractor ever since."#
+got my MSCS from the University of New Mexico
+in 1985 and I've been a contractor and entrepreneur ever since."#
                             }
                             p {
 "In 1989, I founded Abacus Research & Development. We wrote a "
@@ -55,25 +47,16 @@ only had a single floppy drive."#
                             p {
 r#"At ARDI, I got interested in poker. Mat Hostetter's keen insight led the
 two of us to writing what was then the world's fastest poker hand evaluator ("#
-                                a href="https://pdfs.semanticscholar.org/aa66/62b2092e148e7db7004d2030294df51fcb2f.pdf" { "footnotes 113 & 114 on p. 44" } ")."
-                            }
-                            p {
-r#"On Halloween 1998, I wrote the core of "multibot", the first internet
+                                a href="https://pdfs.semanticscholar.org/aa66/62b2092e148e7db7004d2030294df51fcb2f.pdf" { "footnotes 113 & 114 on p. 44" }
+r#"). On Halloween 1998, I wrote the core of "multibot", the first internet
 server to deal multi-table poker tournaments.  I sold multibot to Poker School
 Online who were eventually bought by PokerStars."#
                             }
                             p {
-r#"FWIW, one of
-the founders of PokerStars had actually heard about multibot between when I
-had agreed to sell it to PSO and when I received my check. PokerStars
-offered to pay me twice what I had already agreed on and I turned it down,
-because I had already shaken hands on the deal."#
-                            }
-                            p {
 r#"Multibot was Objective-C, without a framework, just the GNU  Objective-C
 runtime.  Multihreaded, using epoll it was very efficient. However, when PSO
-upgraded to a dual processor, they had an unexplained crash. Wow. I
-discovered "#
+upgraded to a dual processor, they had a single crash and asked me to
+investigate. Whee! I discovered "#
                                 a href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=9969" { "a race condition" }
 " in the Objective-C runtime."
                             }
@@ -88,34 +71,31 @@ in Albuquerque.  In 2009, "#
                             p {
                                 "Since then, I've been doing business as "
                                     span.devctm { "devctm" }
-                                r#", a name meant to imply that I'm a coding "machine". However with almost all of my
-recent work in private repositories, I don't have much code
-to show.  On the other hand, I started running in 2009 and have participated
-in many
-                                    "#
-                                    a href="https://ultrasignup.com/results_participant.aspx?fname=Clifford&lname=Matthews" { "running events" }
-                                r#", so perhaps we can stipulate that I'm
-a running machine and that I am driven.
-                                    "#
+r#", a name meant to imply that I'm a coding "machine". However with almost all
+of my recent work in private repositories, I don't have much code
+to show.  On the other hand, I "#
+                                    a href="https://ultrasignup.com/results_participant.aspx?fname=Clifford&lname=Matthews" { "run a lot" }
+", so let's stipulate that I'm extremely internally motivated."
                             }
                             p {
-r#"I'm currently seeking contract work as a Rust programmer, even though I'm
-new to the language and not particularly good or efficient.  As such, for now,
-I'm better suited to a supporting role rather than a lead architect.  I'm
-inspired by what some people are doing with Rust and I'll certainly be doing
-a lot of side projects in Rust, even if I have to program in Ruby (which I
-still love) to pay the bills."#
+r#"I'm seeking work as a Rust programmer, even though I'm not yet nearly as
+good or efficient in Rust as I am in Ruby.  However, I learn quickly and believe
+I am "training my brain" as effectively as I've been "#
+                                    a href="https://github.com/ctm/Bataan-Memorial-Death-March" { "training my body" }
+".  My Rust will improve significantly."
                             }
                             p {
                                 "This page is a "
-                                a href="https://github.com/ctm/devctm" { "tiny Rust program" }
+                                a href="https://github.com/ctm/devctm" { "Rust program" }
 " using "
                                     a href="https://actix.rs" { "actix" }
                                 ", "
                                     a href="https://maud.lambda.xyz" { "maud" }
-                                " and "
+                                "and "
                                     a href="https://github.com/kaj/rsass" { "rsass" }
-                                "."
+                                ". I wrote "
+                                    a href="https://github.com/ctm/actix-web-lets-encrypt" { "actix-web-lets-encrypt" }
+                                " for https support."
                             }
                         }
                         div#contact {
